a116d6b5d0c0595d1876f2656581c2145ba1b0a2
carogigena <130092105+carogigena@users.noreply.github.com>
2023-11-19 00:41:56 -0300

    Delete TPFinal/routes/tienda.js

--- TPFinal/routes/tienda.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Titulo', message: '¡Hola curso de Javascript!' });
-});
-
-module.exports = router;
